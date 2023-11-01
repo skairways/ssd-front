0b00b53236bef0dab29318a16dd767c58c048507
Behruz <behruz@bictory.io>
2023-11-01 16:44:19 +0500

    setup pages route

--- src/templates/next/next.config.js
+++ src/templates/next/next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
+const path = require("path")
+
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	webpack: (config) => {
+		config.resolve.alias["@"] = path.join(__dirname, "src", "app")
+		return config
+	},
+	rewrites() {
+		return [
+			{
+				source: "/",
+				destination: "/main",
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
