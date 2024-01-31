5766403ac95484b101355e41cbf98a509d22fad1
Sultonbek <kingsulton45@gmail.com>
2024-01-31 16:34:10 +0500

    ts error overlay removed

--- src/examples/React/vite.config.ts
+++ src/examples/React/vite.config.ts
@@ -1,13 +1,12 @@
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vite"
-import checker from "vite-plugin-checker"
 import svgr from "vite-plugin-svgr"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths(), svgr(), checker({ typescript: true })],
+	plugins: [react(), tsconfigPaths(), svgr()],
 	resolve: {
 		alias: {
 			"@styles": path.resolve(__dirname, "src/shared/styles"),
