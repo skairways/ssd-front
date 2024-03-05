e4bca7f37f7e35efcccae5b4398116d13140c802
Abrorxon <abror.karimxonov@ssd.uz>
2024-03-05 16:33:01 +0500

    added error boundary package, removed doundary.tsx file, changed route file for error boundary

--- src/examples/React/src/app/lib/routing.tsx
+++ src/examples/React/src/app/lib/routing.tsx
@@ -6,31 +6,38 @@ import HomePage from "@/pages/home"
 import NotFoundPage from "@/pages/not-found"
 
 import { AppPages } from "@/shared/constants/routes"
+import { ErrorBoundary } from "react-error-boundary";
+import {CodeErrorComponent} from "@/shared/ui/errors";
 
 export const router = createBrowserRouter([
 	{
-		path: AppPages.RootPage,
-		element: <HomePage />,
-	},
-	{
-		path: AppPages.ExamplePage,
+		element:   <ErrorBoundary FallbackComponent={CodeErrorComponent}/>,
 		children: [
 			{
-				index: true,
-				element: <ExamplePage />,
+				path: AppPages.RootPage,
+				element: <HomePage />,
 			},
 			{
-				path: ":id",
-				element: <ArticlePage />,
+				path: AppPages.ExamplePage,
+				children: [
+					{
+						index: true,
+						element: <ExamplePage />,
+					},
+					{
+						path: ":id",
+						element: <ArticlePage />,
+					},
+				],
 			},
-		],
-	},
-	{
-		path: AppPages.NotFoundPage,
-		element: <NotFoundPage />,
-	},
-	{
-		path: AppPages.AnyPage,
-		element: <Navigate replace to={AppPages.NotFoundPage} />,
-	},
+			{
+				path: AppPages.NotFoundPage,
+				element: <NotFoundPage />,
+			},
+			{
+				path: AppPages.AnyPage,
+				element: <Navigate replace to={AppPages.NotFoundPage} />,
+			},
+		]
+	}
 ])
